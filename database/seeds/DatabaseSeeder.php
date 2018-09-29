0717bf8219d54337ab6fa661ed9d795b277c4c7f
brianbronej <brianbronej@me.com>
2018-09-29 16:52:39 +0300

    new changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,5 +22,16 @@ class DatabaseSeeder extends Seeder
     App\Role::create([
             'name' => 'Director of Studies'
         ]);
+        App\StudentGroup::create(
+          [
+            'name'=>'OLevel'
+          ]
+        );
+        App\StudentGroup::create(
+          [
+            'name'=>'Alevel'
+          ]
+        );
     }
+
 }
